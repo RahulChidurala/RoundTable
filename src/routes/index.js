9200087fbf6496f3268efa000d2b1e950f865d43
JonSousa <interactiveemerald@gmail.com>
2017-03-16 21:21:57 -0500

    Do not use squash, just had to fix all the mistakes I made.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-
+ let number = Math.floor((Math.random() * 10) + 1); //Chooses a number between 1 and 10
   res.render('index', {
+    number: number,
     pageTitle: 'Home',
     pageID: 'home'
   });
