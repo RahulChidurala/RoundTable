d69210158965097d6c23841ed7cd672493adfe32
JonSousa <interactiveemerald@gmail.com>
2017-03-16 21:06:02 -0500

    Added a character sheet and a random number generator to the index route.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-
+ let number = Math.random();
   res.render('index', {
+    number: number,
     pageTitle: 'Home',
     pageID: 'home'
   });
